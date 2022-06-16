bd43c4982950f1596be9857b4a11d6e91674cdc3
santocaridi99 <santocaridi99@gmail.com>
2022-06-16 15:38:54 +0200

    fix percorso  route Cors

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod();
+            policy.WithOrigins("https://localhost:3000").AllowAnyHeader().AllowAnyMethod();
         });
 });
 // Add services to the container.
