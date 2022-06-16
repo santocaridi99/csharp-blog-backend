6200255aa313ba9a6d51be2140a6a1b908e65256
santocaridi99 <santocaridi99@gmail.com>
2022-06-16 13:04:46 +0200

    aggiunto Cors

--- Program.cs
+++ Program.cs
@@ -2,13 +2,24 @@ using Microsoft.EntityFrameworkCore;
 using csharp_blog_backend.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
+// MODIFICA AGGIUNTA
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod();
+        });
+});
 // Add services to the container.
 
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<BlogContext>(opt =>
     opt.UseInMemoryDatabase("posts"));
+
+
+
 
 var app = builder.Build();
 
@@ -17,6 +28,8 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors();
 
 app.MapControllers();
 
